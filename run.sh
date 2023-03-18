37581947c41d708141d18c3db2ce95010141a034
SkollRyu <sixmilk0128@gmail.com>
2023-03-18 19:49:07 +0000

    feat: experiment 2 coding done

--- run.sh
+++ run.sh
@@ -39,24 +39,38 @@ rm -rf experiment1/scheduler/SJF/output/*
 
 
 # Input Generate
-for (( i = 1; i <= $test_cases; i++ ))
+sed -i "s/meanCpuBurst=[0-9]*\.[0-9]*/meanCpuBurst=10.0/g" experiment1/input_parameters.prp
+sed -i "s/meanCpuBurst=[0-9]*\.[0-9]*/meanIOBurst=10.0/g" experiment2/input_parameters.prp
+
+for (( i = 1; i <= $((test_cases * 2)); i++ ))
 do
+    if [ $i -eq 15 ]; then
+        sed -i "s/meanCpuBurst=[0-9]*\.[0-9]*/meanCpuBurst=20.0/g" experiment1/input_parameters.prp
+        sed -i "s/meanCpuBurst=[0-9]*\.[0-9]*/meanIOBurst=20.0/g" experiment2/input_parameters.prp
+    fi
     java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment1/input_parameters.prp experiment1/inputs/inputs${i}.in
+    java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/input_parameters.prp experiment2/inputs/inputs${i}.in
     newSeed=$((RANDOM%(999999999999999-100000000000000+1)+100000000000000))
     sed -i "s/seed=[0-9]*/seed=$newSeed/g" experiment1/input_parameters.prp
-    # java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment2/input_parameters.prp experiment2/inputs/inputs${i}.in
+    sed -i "s/seed=[0-9]*/seed=$newSeed/g" experiment2/input_parameters.prp
     # java -cp target/os-coursework1-1.0-SNAPSHOT.jar InputGenerator experiment3/input_parameters.prp experiment3/inputs/inputs${i}.in
 done
 
 
 # Run Simulation
-for (( i = 1; i <= $test_cases; i++ ))
+for (( i = 1; i <= $((test_cases * 2)); i++ ))
 do
     # exp 1
     java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/scheduler/FCFS/simulator_parameters.prp experiment1/scheduler/FCFS/output/output${i}.out experiment1/inputs/inputs${i}.in
     java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/scheduler/ISJF/simulator_parameters.prp experiment1/scheduler/ISJF/output/output${i}.out experiment1/inputs/inputs${i}.in
     java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/scheduler/RR/simulator_parameters.prp experiment1/scheduler/RR/output/output${i}.out experiment1/inputs/inputs${i}.in
     java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment1/scheduler/SJF/simulator_parameters.prp experiment1/scheduler/SJF/output/output${i}.out experiment1/inputs/inputs${i}.in
+
+    # exp 2
+    java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/scheduler/FCFS/simulator_parameters.prp experiment2/scheduler/FCFS/output/output${i}.out experiment2/inputs/inputs${i}.in
+    java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/scheduler/ISJF/simulator_parameters.prp experiment2/scheduler/ISJF/output/output${i}.out experiment2/inputs/inputs${i}.in
+    java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/scheduler/RR/simulator_parameters.prp experiment2/scheduler/RR/output/output${i}.out experiment2/inputs/inputs${i}.in
+    java -cp target/os-coursework1-1.0-SNAPSHOT.jar Simulator experiment2/scheduler/SJF/simulator_parameters.prp experiment2/scheduler/SJF/output/output${i}.out experiment2/inputs/inputs${i}.in
 done
 
 #!/bin/bash
